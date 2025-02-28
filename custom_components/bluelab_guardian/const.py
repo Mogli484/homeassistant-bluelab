4891a87067eedd2858e266458fc4de7db0c5787a
Mogli484 <mogli2301@googlemail.com>
2025-02-28 19:13:16 +0100

    Update const.py

--- custom_components/bluelab_guardian/const.py
+++ custom_components/bluelab_guardian/const.py
@@ -1,8 +1,8 @@
 from datetime import timedelta
 
 DOMAIN = "bluelab_guardian"
-CONF_API_TOKEN = "api_token"
-CONF_ORGANIZATION_ID = "organization_id"
+CONF_API_TOKEN = "ed_chws4gyaa939b0gd7pbnotfgj83lgp7hqfvy8wll0cvk21r3uzsvbmxclpbpv9hv"
+CONF_ORGANIZATION_ID = "98a0a700-f5f6-11ef-a5ae-8dff4b34f2dc"
 DEVICE_LIST_URL = "https://api.edenic.io/api/v1/device/"
 TELEMETRY_URL = "https://api.edenic.io/api/v1/telemetry/"
 DEVICE_ATTRIBUTE_URL = "https://api.edenic.io/api/v1/device-attribute/"
